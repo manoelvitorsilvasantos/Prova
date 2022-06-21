ca3cc157e9485cd7f7342d9fc125bae05f833fae
manoel vitor <mvictordossantos.limite@gmail.com>
2022-06-21 18:11:28 -0300

    build: update class Main

--- src/br/app/main/Main.java
+++ src/br/app/main/Main.java
@@ -13,84 +13,76 @@ import java.util.Scanner;
  */
 public class Main {
     
-    private static final Runnable questao1 =  () -> {
-        int INDICE = 13, SOMA = 0, K = 0;
-        do{
-            K += 1;
-            SOMA = SOMA + K;
-        }while(K < INDICE);
-        System.out.println("a variável SOMA?  >> "+String.valueOf(SOMA));
-    };
     
-    private static Runnable questao2 = ()->{
-        int n = 0, ultimo = 1, penultimo = 1;
+    public Main(){
+        Scanner leitura = new Scanner(System.in);
+        int opc = 0;
+        while(true)
+        {
+            System.out.println("Digite uma opção >> ");
+            opc = leitura.nextInt();
         
-        Scanner read = new Scanner(System.in);
-        n = read.nextInt();
-        int termo = 0;
-        if ( n == 1 || n == 2){
-            System.out.println(1);
-        }else{
-            int contador = 3;
-            while (contador <= n){
-                termo = ultimo + penultimo;
-                penultimo = ultimo;
-                ultimo = termo;
-                contador += 1;
-            }
-            System.out.println(termo);
-        }
-    /*            count=3
-    while count <= n:
-        termo = ultimo + penultimo
-        penultimo = ultimo
-        ultimo = termo
-        count += 1
-    print(termo)*/
-        
-    };
-    
-    private static Runnable questao3 = ()->{
-    };
-    private static Runnable questao4 = ()->{
-    };
-    private static Runnable questao5 = ()->{
-    };
-
-    public static void main(String[] args)  
-    {
-        Scanner leitor = new Scanner(System.in);
-        int opc = -1;
-        while(opc != 0){
-            System.out.println("Digite uma opção: ");
-            opc = leitor.nextInt();
-            System.out.printf("Escolheu a opção %d\n", opc);
             switch(opc)
             {
                 case 0:
                     System.exit(0);
                     break;
                 case 1:
-                    new Thread(questao1).start();
-                    
+                    questao1();
                     break;
                 case 2:
-                    new Thread(questao2).start();
+                    questao2();
                     break;
                 case 3:
-                    new Thread(questao3).start();
+                    questao3();
                     break;
                 case 4:
-                    new Thread(questao4).start();
+                    questao4();
                     break;
                 case 5:
-                    new Thread(questao5).start();
+                    questao5();
                     break;
                 default:
-                    System.out.println("Opção Inválida!");
+                    System.out.println("Opção Inválida!!!");
                     break;
             }
         }
+    }
+    
+    public void questao1(){
+        int INDICE = 13, SOMA = 0, K = 0;
+        do{
+            K += 1;
+            SOMA = SOMA + K;
+        }while(K < INDICE);
+        System.out.println("a variável SOMA?  >> "+String.valueOf(SOMA));
+    }
+    
+    public void questao2(){
+       
+    }
+    
+    public void questao3(){
+        System.out.println("Ainda não está pronta!!!");
+    }
+    
+    public void questao4(){
+        System.out.println("Ainda não está pronta!!!");
+    }
+    
+    public void questao5(){
+        System.out.println("Ainda não está pronta!!!");
+    }
+    
+
+    public static void main(String[] args)  
+    {
+       new Thread(){
+           @Override
+           public void run(){
+               Main main = new Main();
+           }
+       }.start();
     }
     
 }
